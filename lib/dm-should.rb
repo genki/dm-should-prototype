ce871d627c84fef94493560ff3e82fb5112aeb06
yukiko <yu0420@gmail.com>
2009-04-18 19:05:57 +0900

    set the 1st goal.

--- lib/dm-should.rb
+++ lib/dm-should.rb
@@ -4,3 +4,23 @@ require 'rubygems'
 require 'dm-core'
 
 dir = Pathname(__FILE__).dirname.expand_path / 'dm-should'
+
+module DataMapper
+  module Model
+    
+    def property_with_spec(*args, &block)
+      property(*args)
+    end
+
+  end
+end
+
+module DataMapper
+  module Resource
+
+    def valid?
+      true
+    end
+
+  end
+end
--- spec/dm-should_spec.rb
+++ spec/dm-should_spec.rb
@@ -1,7 +1,30 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "dm-should" do
-  it "should do nothing" do
-    true.should == true
+class Item
+  include DataMapper::Resource 
+
+  property :id, Serial
+  property_with_spec :name, String do
+    should.be_present
+  end
+
+end
+
+describe "DataMapper::Resource with dm-should" do
+  it "could check presence of attributes" do
+
+    record = Item.new
+
+    # When record.name is nil
+    record.valid?.should == false
+
+    # When record.name is empty string.
+    record.name = ""
+    record.valid?.should == false
+
+    # When record.name is string which is not empty.
+    reocrd.name = "foo"
+    record.valid?.should == true
+
   end
 end
