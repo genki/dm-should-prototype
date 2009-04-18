93d83d0913121f7b0ee36857e4aa6e2d78eb975d
yukiko <yu0420@gmail.com>
2009-04-19 04:44:46 +0900

    thinking about how specs are collected and how ensure_specs method is defined.
    * property_with_spec method does 3 things now.
    1. define a property by using the original property method.
    2. collect specs by using given block.
    3. define ensure_specs method.
    
    * AModel.specs returns DataMapper::Should::SpecCollection class.
    
    * a_record.errors returns DataMapper::Should::Errors class.
    
    * there will be spec classes as subclasses of DataMapper::Should::SpecBase,
    like the DataMapper::Should::BePresent class.

--- lib/dm-should.rb
+++ lib/dm-should.rb
@@ -8,8 +8,25 @@ dir = Pathname(__FILE__).dirname.expand_path / 'dm-should'
 module DataMapper
   module Model
     
+    attr_reader :specs
+    
+
     def property_with_spec(*args, &block)
-      property(*args)
+      pro = property(*args)
+      collect_specs(pro, block)
+      define_ensure_specs_method(pro)
+    end
+
+
+    def collect_specs(pro, spec_proc)
+      @specs = Should::SpecCollection.new(pro)
+      @specs.collect spec_proc
+    end
+
+    def define_ensure_specs_method(pro)
+      code =   "def ensure_specs\n"
+      code <<  "end"
+      pro.model.module_eval(code, __FILE__, __LINE__) 
     end
 
   end
@@ -18,9 +35,110 @@ end
 module DataMapper
   module Resource
 
+
     def valid?
-      true
+      errors.empty?
+    end
+
+
+    def errors
+      @errors = Should::Errors.new unless @errors
+      @errors
+    end
+
+
+  end
+end
+
+require dir / "version"
+
+# how are specs collected ?
+module DataMapper::Should
+
+  class SpecCollection
+
+    attr_reader :specs, :property
+
+    def initialize(property)
+      @property = property
+      @specs = []
+    end
+    
+    def collect(spec_proc)
+      SpecCollector.collect(self, spec_proc)
+      specs
+    end
+
+    def inspect
+      specs.inspect
+    end
+
+    def to_ary
+      specs.dup
+    end
+
+
+    class SpecCollector
+
+      def self.collect(collection, spec_proc)
+        obj = self.new(collection)
+        obj.instance_eval &spec_proc if spec_proc.is_a? Proc
+      end
+
+      def initialize(collection)
+        @collection = collection
+        @specs = []
+      end
+
+      def should(spec)
+        spec.property = @collection.property
+        @collection.specs << spec
+      end
+
+      def be_present
+        BePresent.new
+      end
     end
 
   end
 end
+
+# spec classes
+module DataMapper::Should
+  
+  class SpecBase
+
+    attr_accessor :property
+
+    def self.name(new_value=nil)
+      @name = new_value if new_value.is_a? Symbol
+      @name
+    end
+
+  end
+
+  class BePresent < SpecBase
+    name :be_present
+  end
+end
+
+
+# core ext for be_present
+unless Object.respond_to? :present?
+  class Object
+    def presnet?
+      !blank?
+    end
+  end
+end
+
+
+# how record.errors could be?
+module DataMapper::Should
+  class Errors
+    def empty?
+      true
+    end
+  end
+end
+
--- spec/dm-should_spec.rb
+++ spec/dm-should_spec.rb
@@ -5,26 +5,56 @@ class Item
 
   property :id, Serial
   property_with_spec :name, String do
-    should.be_present
+    should be_present
   end
 
 end
 
 describe "DataMapper::Resource with dm-should" do
-  it "could check presence of attributes" do
 
-    record = Item.new
+  subject do
+    @record
+  end
+
+  before do
+    @record = Item.new
+  end
+
+
+  it "should respond_to :ensure_specs" do
+    should respond_to(:ensure_specs)
+  end
+
+
+  it "should have specs as a class attribute" do
+    Item.specs.should be_a(DataMapper::Should::SpecCollection)
+    Item.specs.to_ary.should have(1).items
+    Item.specs.to_ary.first.class.name.should == :be_present
+  end
+
+
+  it "could check presence of attributes" do
+    pending "the 1st goal"
 
     # When record.name is nil
-    record.valid?.should == false
+    @record.valid?.should == false
 
-    # When record.name is empty string.
-    record.name = ""
-    record.valid?.should == false
+    # When @record.name is empty string.
+    @record.name = ""
+    @record.valid?.should == false
 
-    # When record.name is string which is not empty.
+    # When @record.name is string which is not empty.
     reocrd.name = "foo"
-    record.valid?.should == true
+    @record.valid?.should == true
 
   end
+
+end
+
+describe "Spec Classes of DataMapper::Should" do
+
+  it "should has its symbolized name as an attribute" do
+    DataMapper::Should::BePresent.name.should == :be_present
+  end
+
 end
