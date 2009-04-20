c487b038d00bd3effb4539102283d11cefa04cd8
yukiko <yu0420@gmail.com>
2009-04-20 09:06:13 +0900

    An idea of DataMapper::Should::SpecCollector.

---
+++ lib/dm-should/spec_collector.rb
@@ -0,0 +1,34 @@
+module DataMapper
+  module Should
+    class SpecCollector
+
+      def self.collect(property, spec_proc)
+        obj = self.new(property)
+        obj.instance_eval &spec_proc if spec_proc.is_a? Proc
+        obj.collected
+      end
+
+      def initialize(property)
+        unless property.is_a? Property
+          raise "the argument of SpecCollector.new should be an Property"
+        end
+        @property = property
+        @specs = []
+      end
+
+      def should(spec)
+        spec.property = @property
+        @specs << spec
+      end
+
+      def collected
+        @specs
+      end
+
+      def be_present
+        BePresent.new
+      end
+
+    end
+  end
+end
