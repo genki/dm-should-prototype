5b8024ac1b4c47deb6e8099b3e9da50b7042dd90
yukiko <yu0420@gmail.com>
2009-04-28 11:04:59 +0900

    [DataMapper::Should::Translation] write all version of specdocs in one hash.

--- lib/dm-should/translation.rb
+++ lib/dm-should/translation.rb
@@ -1,40 +1,39 @@
 module DataMapper::Should
   class Translation 
 
-    cattr_accessor :error_messages_like_rspec
-    self.error_messages_like_rspec = {
-      :be_present => "expected %{field} was present, got %{actual}",
-      :be_unique  => "expected %{field} was unique, got %{actual}",
-      :be_positive_integer => "expected %{field} was positive integer, got %{actual}"
-    }.to_mash
+    cattr_accessor :translations
+    self.translations = {
+      :warn_like_rspec => {
+        :be_present => "expected %{field} was present, got %{actual}",
+        :be_unique  => "expected %{field} was unique, got %{actual}",
+        :be_positive_integer => "expected %{field} was positive integer, got %{actual}"
+      },
 
-    cattr_accessor :error_messages_like_rails
-    self.error_messages_like_rails = {
-      :be_present => "%{field} can't be blank", 
-      :be_unique  => "%{actual} has already been taken",
-      :be_positive_integer => "%{actual} is not a positive number"
-    }.to_mash
+      :warn_like_rails => {
+        :be_present => "%{field} can't be blank", 
+        :be_unique  => "%{actual} has already been taken",
+        :be_positive_integer => "%{actual} is not a positive number"
+      },
 
-    cattr_accessor :error_messages_like_dm_validations
-    self.error_messages_like_dm_validations = {
-      :be_present => "%{field} must not be blank",
-      :be_unique  => "%{actual} is already taken",
-      :be_positive_integer => "%{field} must be a positive number"
-    }.to_mash
+      :warn_like_dm_validations => {
+        :be_present => "%{field} must not be blank",
+        :be_unique  => "%{actual} is already taken",
+        :be_positive_integer => "%{field} must be a positive number"
+      },
 
-    cattr_accessor :specdocs
-    # TODO: should or must, which is better?
-    # - %{field} should be present
-    # - %{field} must be present
-    self.specdocs = {
-      :be_present => "%{field} should be present",
-      :"be_unique" => "%{field} should be unique",
-      :"be_unique_with_scopes" => "%{field} should be unique (scope: %{scopes})",
-      :be_positive_integer => "%{field} should be a positive number"
+      # TODO: should or must, which is better?
+      # - %{field} should be present
+      # - %{field} must be present
+      :specdocs => {
+        :be_present => "%{field} should be present",
+        :"be_unique" => "%{field} should be unique",
+        :"be_unique_with_scopes" => "%{field} should be unique (scope: %{scopes})",
+        :be_positive_integer => "%{field} should be a positive number"
+      }
     }.to_mash
 
     def self.translate(scope, values={})
-      String.new(specdocs[scope]) % values
+      String.new(translations[:specdocs][scope]) % values
     end
 
   end
