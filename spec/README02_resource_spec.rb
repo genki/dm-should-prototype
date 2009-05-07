f0b4f34b28b977ba397c23298452928d0b4a2d42
yukiko <yu0420@gmail.com>
2009-05-07 11:38:24 +0900

    start writing specs for README

---
+++ spec/README02_resource_spec.rb
@@ -0,0 +1,68 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/rdoc_helper'
+
+describe "=== DataMapper::Resource (Instance methods) with dm-should" do
+  h4 "valid? method" do
+    before(:each) {  @item = Item.new  }
+    attr_reader :item
+
+    it "should return true if a record satisfy its specs and false if it doesn't" do
+
+      item.valid?.should be_false
+
+      item.name = "Name"
+      item.valid?.should be_true
+
+    end
+
+    it "a record shouldn't be saved when valid? returns false" do
+      Item.auto_migrate!
+
+      item.valid?.should be_false
+      proc { item.save }.should_not change(Item,:count)
+
+      item.name = "Name"
+      item.valid?.should be_true
+      proc { item.save }.should change(Item, :count).by(1)
+
+    end
+
+  end
+end
+
+h4 "errors method" do
+  before(:each) {  @record = Item.new  }
+  attr_reader :record
+
+  it "should return an instance of DataMapper::Should::Errors."  do
+    record.errors.should be_a(DataMapper::Should::Errors)
+  end
+
+  it "Once valid? method executed and return false, record.errors has details about it" do
+    record.valid?
+    record.errors.empty?.should == false
+  end
+
+  it "You can generate an Array of error messages by passing a block to translation_keys_each method:
+    record.errors.translation_keys_each do |translation_key, assigns|
+      your_translation_system.translate(translation_key, assigns)
+    end
+
+  The arguments of the block is translation_key and assigns:
+  [<b>translation_key</b>]
+    a translation_key is kind of 'warn.be_present' or 'warn.be_unique'.
+    They are combinations of 'doctype.spectype'.
+  [<b>assigns[:field]</b>]
+    is now a String like 'Modelname.fieldname'. this is also supposed to be translated 
+  [<b>assigns[:actual]</b>]
+    is an actual value at the recently validated field 
+  " do 
+    record.valid?
+    record.errors.translation_keys_each do |translation_key, assigns|
+      translation_key.should == "warn.be_present"
+      assigns.should have_key(:actual)
+      assigns.should have_key(:field)
+      assigns[:field].should == "Item.name"
+    end
+  end
+end
